a40e6f5830992c000cf5b2d04f4f3eb8f06ad884
facheng.wang <facheng.wang@otms.cn>
2018-11-24 20:42:25 +0800

    first

---
+++ first/src/main/java/com/template/Application.java
@@ -0,0 +1,27 @@
+package com.template;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 启动程序
+ * 
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@MapperScan("com.template.project.*.*.mapper")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ first/src/main/java/com/template/project/system/user/controller/IndexController.java
@@ -0,0 +1,48 @@
+package com.template.project.system.user.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.template.framework.config.ProjectConfig;
+import com.template.framework.web.controller.BaseController;
+import com.template.project.system.menu.domain.Menu;
+import com.template.project.system.menu.service.IMenuService;
+import com.template.project.system.user.domain.User;
+
+/**
+ * 首页 业务处理
+ * 
+ */
+@Controller
+public class IndexController extends BaseController
+{
+    @Autowired
+    private IMenuService menuService;
+
+    @Autowired
+    private ProjectConfig projectConfig;
+
+    // 系统首页
+    @GetMapping("/index")
+    public String index(ModelMap mmap)
+    {
+        // 取身份信息
+        User user = getSysUser();
+        // 根据用户id取出菜单
+        List<Menu> menus = menuService.selectMenusByUser(user);
+        mmap.put("menus", menus);
+        mmap.put("user", user);
+        mmap.put("copyrightYear", projectConfig.getCopyrightYear());
+        return "index";
+    }
+
+    // 系统介绍
+    @GetMapping("/system/main")
+    public String main(ModelMap mmap)
+    {
+        mmap.put("version", projectConfig.getVersion());
+        return "main";
+    }
+}
