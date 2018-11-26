02e92ab0fb52f250504943c319e2af7e23037525
facheng.wang <facheng.wang@otms.com>
2018-11-26 18:14:22 +0800

    for postgresql init.

---
+++ second/src/main/java/com/template/Application.java
@@ -0,0 +1,26 @@
+package com.template;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 启动程序
+ * 
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@MapperScan("com.template.project.*.*.mapper")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ second/src/main/java/com/template/framework/web/page/TableSupport.java
@@ -0,0 +1,27 @@
+package com.template.framework.web.page;
+
+import com.template.common.utils.ServletUtils;
+import com.template.common.constant.Constants;
+
+/**
+ * 表格数据处理
+ * 
+ */
+public class TableSupport {
+    /**
+     * 封装分页对象
+     */
+    public static PageDomain getPageDomain() {
+        PageDomain pageDomain = new PageDomain();
+        pageDomain.setPageNum(ServletUtils.getParameterToInt(Constants.PAGE_NUM));
+        pageDomain.setPageSize(ServletUtils.getParameterToInt(Constants.PAGE_SIZE));
+        pageDomain.setOrderByColumn(ServletUtils.getParameter(Constants.ORDER_BY_COLUMN));
+        pageDomain.setIsAsc(ServletUtils.getParameter(Constants.IS_ASC));
+        return pageDomain;
+    }
+
+    public static PageDomain buildPageRequest() {
+        return getPageDomain();
+    }
+
+}
